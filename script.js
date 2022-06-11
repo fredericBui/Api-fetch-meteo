3aea99667e88b1220dd501a8c44b21222fd164b4
Frederic_Bui <bui.frederic.pro@gmail.com>
2022-06-11 20:26:25 +0000

    ajout fonctionnalité temp actuel

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function temp(latitude,longitude){
     return response.json();
   })
   .then(function(data) {
-    console.log(data.current_weather.temperature);
     document.getElementById("root").innerHTML = data.current_weather.temperature + "°C";
+    weather = weatherCase(data);
+    document.getElementById("weather").innerHTML = weather;
   });
 }
 
@@ -19,7 +20,53 @@ function customTemp(){
     return response.json();
   })
   .then(function(data) {
-    console.log(data.current_weather.temperature);
     document.getElementById("customTemp").innerHTML = data.current_weather.temperature + "°C";
+    customWeather = weatherCase(data);
+    document.getElementById("customWeather").innerHTML = customWeather;
   });
+}
+
+function weatherCase(props){
+  switch (props.current_weather.weathercode){
+    case 0 :
+      return "Clear sky";
+    case 1 :
+    case 2 :
+    case 3 :
+      return "Clear sky";
+    case 45 :
+    case 48 :
+      return "Fog and depositing rime fog";
+    case 51 :
+    case 53 :
+    case 55 :
+      return "Drizzle: Light, moderate, and dense intensity";
+    case 56 :
+    case 57 :
+      return "Freezing Drizzle: Light and dense intensity";
+    case 61 :
+    case 63 :
+    case 65 :
+      return "Rain: Slight, moderate and heavy intensity";
+    case 66 :
+    case 67 :
+      return "Freezing Rain: Light and heavy intensity";
+    case 71 :
+    case 73 :
+    case 75 :
+      return "Snow fall: Slight, moderate, and heavy intensity";
+    case 77 :
+      return "Snow grains";
+    case 80 :
+    case 81 :
+    case 82 :
+      return "Rain showers: Slight, moderate, and violent";
+    case 95 :
+      return "Thunderstorm: Slight or moderate";
+    case 96 :
+    case 99 :
+      return "Thunderstorm with slight and heavy hail";
+    default :
+      return "Inconnu";
+    };
 }
